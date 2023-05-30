d6b8cd7d36a62163d30fbf580bce8938982e629b
Md Asaduzaman <asadasad9282@gmail.com>
2023-05-30 09:39:26 +0600

    setup express cors parser mongoose

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
+import cors from "cors"
+
+
+
+// parser
+app.use(cors())
+app.use(express.urlencoded({ extended: true }))
+
+
+
+// GET method route
+app.get('/', (req: Request, res: Response) => {
+    res.send('GET request to the homepage')
+})
+
+
+export default app
