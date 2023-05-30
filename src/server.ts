946fc703dba97e433b89d413b77f567a2433d518
Md Asaduzaman <asadasad9282@gmail.com>
2023-05-30 16:56:49 +0600

    Setup exlint prettier huski lint stage

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,20 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config";
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function main() {
-    console.log(config);
+  console.log(config)
 
-    try {
-        mongoose.connect(config.database_url as string);
-        console.log('database connection successfull');
+  try {
+    mongoose.connect(config.database_url as string)
+    console.log('database connection successfull')
 
-        app.listen(config.port, () => {
-            console.log(`express app is listining in port ${config.port}`);
-
-        })
-
-
-    } catch (error) {
-        console.log('Failed to connect database', error);
-
-    }
+    app.listen(config.port, () => {
+      console.log(`express app is listining in port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Failed to connect database', error)
+  }
 }
 
 main()
