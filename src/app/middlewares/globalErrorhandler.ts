e558d59b08b6fdc37bd6a7e9afb3e09038ff0326
Md Asaduzaman <asadasad9282@gmail.com>
2023-06-04 23:39:59 +0600

    make comon error pattern

--- src/app/middlewares/globalErrorhandler.ts
+++ src/app/middlewares/globalErrorhandler.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from 'express'
+import { IGenericErrorMessage } from '../../interfaces/error'
+import config from '../../config'
 
 // global error handler
 const globalErrorHandler = (
@@ -7,9 +9,18 @@ const globalErrorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(400).json({
-    appErro: err,
+  const statusCode = 500
+  const message = 'Something went wrong'
+  const errorMessages: IGenericErrorMessage[] = []
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    errorMessages,
+    stack: config.env !== 'production' ? err?.stack : undefined,
   })
+
   next()
 }
+
 export default globalErrorHandler
